260d6f3ca5fabcef90a3ed779c138d348f03bc4e
wicksand <wicksand@byui.edu>
2025-04-30 21:49:31 -0600

    fixed Javascript and added change img

--- mission/java.js
+++ mission/java.js
@@ -1,5 +1,5 @@
 let selectElm = document.querySelector('select');
-let logo = debuggerocument.querySelector('img');
+let logo = document.querySelector('img');
 
 selectElm.addEventListener('change', changeTheme);
 
@@ -7,10 +7,16 @@ function changeTheme(){
     let currentTheme = selectElm.value;
     if(currentTheme == 'dark'){
         //change body to dark
+        const body = document.querySelector('body');
+        body.classList.add("dark")
         //change logo to dark logo
+        logo.setAttribute('src','byui-logo_white.png')
 
     } else{
         //remove dark class from body
+        const body = document.querySelector('body');
+        body.classList.remove("dark")
         //change logo to standard
+        logo.setAttribute('src','byui-logo.webp')
     }
 }
