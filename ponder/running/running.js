daa7bd482ab9832a438988e327d6898b1799f91a
wicksand <wicksand@byui.edu>
2025-05-01 20:35:44 -0600

    runninghtml project

---
+++ ponder/running/running.js
@@ -0,0 +1,16 @@
+
+//source code for assingment
+// const apples = 5;
+// const oranges = 3;
+// let total = apples + oranges;
+// console.log("total:", total);
+
+
+//source code 2
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
