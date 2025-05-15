af50603d3cb278978c56ec14a95407e32c365371
wicksand <wicksand@byui.edu>
2025-05-15 16:45:16 -0600

    js debug ponder

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -6,3 +6,17 @@ btn.addEventListener('click', toggleMenu);
 function toggleMenu(){
 menu.classList.toggle('hide');
 }
+
+handleResize(){
+    const menu = document.querySelector(".menu");
+    if (window.innerWidth > 100){
+        menu.classList.remove("hide")
+    }else{
+        menu.classList.add("hide")
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+
---
+++ ponder/Debugging/Debugging.js
@@ -0,0 +1,11 @@
+
+
+const pi = 3.14;
+
+const circleArea = (radius) => {
+let _area = radius * radius * pi;
+return _area;
+}
+
+console.log(circleArea(3));
+console.log(circleArea(4));
