eaf39402e7a1062da019232639b67773c5e789ce
wicksand <wicksand@byui.edu>
2025-05-17 18:11:12 -0600

    menu code fix

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -4,15 +4,15 @@ const menu = document.querySelector('#pages');
 btn.addEventListener('click', toggleMenu);
 
 function toggleMenu(){
-menu.classList.toggle('hide');
+    menu.classList.toggle('hide');
 }
 
-handleResize(){
-    const menu = document.querySelector(".menu");
-    if (window.innerWidth > 100){
-        menu.classList.remove("hide")
+function handleResize(){
+    const menu = document.querySelector("#pages");
+    if (window.innerWidth > 1000){
+        pages.classList.remove("hide");
     }else{
-        menu.classList.add("hide")
+        menu.classList.add("hide");
     }
 }
 
